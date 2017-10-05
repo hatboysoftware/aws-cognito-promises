e28783c5a78e5ec2049d99cd0a4aefe80ed60da0
Matthew Alan Gray <mgray@hatboysoftware.com>
2017-10-05 18:37:21 -0500

    Refactored to support usernames and attributes during signup

--- src/register.js
+++ src/register.js
@@ -1,18 +1,10 @@
-import { CognitoUserAttribute } from 'amazon-cognito-identity-js'
-
 import { getUserPool } from './config'
 
-export default function(email, password) {
-  const userPool = getUserPool()
+export default function(username, password, attributes) {
+  const userPool = getUserPool();
 
-  const attributeList = [
-    new CognitoUserAttribute({
-      Name: 'email',
-      Value: email
-    })
-  ]
   return new Promise((resolve, reject) => {
-    userPool.signUp(email, password, attributeList, null, (err, result) => {
+    userPool.signUp(username, password, attributes, null, (err, result) => {
       if (err) {
         reject(err)
       } else {
